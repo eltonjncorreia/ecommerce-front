5f409284f21d98839d72ac7fe074594b7ff5b8d9
eltonjncorreia <eltonjncorreia@gmail.com>
2018-10-27 20:58:51 -0300

    deploy app heroku

--- src/app/produto.service.ts
+++ src/app/produto.service.ts
@@ -1,11 +1,12 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import { Produto } from './produtos/produto.iterface';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ProdutoService {
-  produtosURL = "http://localhost:8000/api/v1/produtos/"
+  produtosURL = "https://apiecommerce-trello.herokuapp.com/api/v1/produtos/"
 
   constructor(private http: HttpClient) { }
 

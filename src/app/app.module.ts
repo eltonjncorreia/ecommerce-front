e5ee6a2becba7a0cda530bb7d1fb3d1bc33a1a90
eltonjncorreia <eltonjncorreia@gmail.com>
2018-10-27 15:40:56 -0300

    adcionando service produto ao modulo

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,15 +2,18 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { ProdutosComponent } from './produtos/produtos.component';
+import { ProdutoService } from './produto.service';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ProdutosComponent
   ],
   imports: [
     BrowserModule
   ],
-  providers: [],
+  providers: [ ProdutoService ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
